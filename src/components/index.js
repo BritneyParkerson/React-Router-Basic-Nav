ce31f3b6d0a908bd9d60483e76bb86195aa054a8
Britney Parkerson <justbritneyp1989@gmail.com>
2020-02-13 00:24:47 -0500

    I think I figured it out. Still can't see it...

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,14 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import { BrowserRouter as Router} from "react-router-dom";
 
 export { Home, About, Contact, Navigation };
+
+
+ReactDOM.render(.
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+    );
